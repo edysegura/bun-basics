efa887045b6b8c58f05f0ba0b2824a765b38129f
Edy Segura <edysegura@gmail.com>
2024-05-23 18:20:14 -0300

    chore: update github.api.ts example

--- shell/github.api.ts
+++ shell/github.api.ts
@@ -1,6 +1,9 @@
 import { $ } from 'bun'
 
 const response = await fetch('https://api.github.com/users/edysegura')
-const json = await response.json()
+const data: Record<string, undefined> = await response.json()
 
-await $`echo ${'Name: ' + json.name}`
+await $`echo ${'Name: ' + data.name}`
+await $`echo ${'Public repos: ' + data.public_repos}`
+await $`echo ${'Followers: ' + data.followers}`
+await $`echo ${'Since: ' + data.created_at}`
