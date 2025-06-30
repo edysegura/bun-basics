5c026de111d6fb5691cdcf623937e55414f7d5ba
Edy Segura <edysegura@gmail.com>
2025-06-30 18:58:44 -0300

    feat: implement basic server with HTML serving functionality in TypeScript

---
+++ simple-server/server.ts
@@ -0,0 +1,21 @@
+import { serve } from 'bun'
+
+const PORT = 3000
+const ADDRESS = 'localhost'
+
+serve({
+  port: PORT,
+  hostname: ADDRESS,
+  async fetch(req) {
+    const url = new URL(req.url)
+    if (url.pathname === '/') {
+      const html = await Bun.file('./simple-server/index.html').text()
+      return new Response(html, {
+        headers: { 'Content-Type': 'text/html' },
+      })
+    }
+    return new Response('Not Found', { status: 404 })
+  },
+})
+
+console.log(`Server is up and running at http://${ADDRESS}:${PORT}`)
