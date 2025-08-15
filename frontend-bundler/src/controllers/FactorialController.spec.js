f1af16e735853af2b5a26b0a780eb183ad56f7cf
Edy Segura <edysegura@gmail.com>
2025-08-15 14:59:32 -0300

    feat: add jsdom for testing and implement FactorialController tests
    - Added jsdom as a devDependency for simulating a DOM environment in tests
    - Created FactorialController.spec.js to test the functionality of FactorialController
    - Implemented tests for displaying results, retrieving input values, and calculating factorials

---
+++ frontend-bundler/src/controllers/FactorialController.spec.js
@@ -0,0 +1,82 @@
+import { describe, it, expect, beforeEach, beforeAll } from 'bun:test'
+import { JSDOM } from 'jsdom'
+import { FactorialController } from './FactorialController'
+
+// Set up JSDOM before all tests
+beforeAll(() => {
+  const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
+    url: 'http://localhost',
+  })
+
+  // Make DOM APIs available globally
+  global.document = dom.window.document
+  global.window = dom.window
+  global.navigator = dom.window.navigator
+})
+
+describe('FactorialController.js', () => {
+  let controller
+
+  beforeEach(() => {
+    // Set up document body before each test
+    document.body.innerHTML = `
+      <form action="">
+        <input type="text" id="number">
+        <button type="submit">Calculate</button>
+        <span id="result">0</span>
+      </form>
+    `
+    controller = new FactorialController()
+  })
+
+  it('should show the result on the HTML element', () => {
+    // GIVEN
+    const result = 2
+
+    // AND
+    const htmlElement = document.getElementById('result')
+
+    // WHEN
+    controller.showResult(result)
+
+    // THEN
+    expect(htmlElement.textContent).toBe('2')
+  })
+
+  it('should get a value from input as a number', () => {
+    // GIVEN
+    document.getElementById('number').value = '2'
+
+    // WHEN
+    const number = controller.getNumber()
+
+    // THEN
+    expect(number).toBe(2)
+  })
+
+  it('should calculate factorial from a given number', () => {
+    // GIVEN
+    document.getElementById('number').value = '5'
+
+    // AND
+    const button = document.querySelector('button')
+    const htmlElement = document.getElementById('result')
+
+    // WHEN
+    button.click()
+
+    // THEN
+    expect(htmlElement.textContent).toBe('120')
+  })
+
+  it('should have zero as a default value showing result', () => {
+    // GIVEN
+    const htmlElement = document.getElementById('result')
+
+    // WHEN
+    controller.showResult()
+
+    // THEN
+    expect(htmlElement.textContent).toBe('0')
+  })
+})
