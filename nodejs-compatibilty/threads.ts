c51546834f97a0827f72829d959d52e519eecc42
Edy Segura <edysegura@gmail.com>
2025-08-20 17:17:05 -0300

    feat: add threads example

---
+++ nodejs-compatibilty/threads.ts
@@ -0,0 +1,17 @@
+import {
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData,
+} from 'node:worker_threads'
+
+if (isMainThread) {
+  const data = 'some data'
+  const worker = new Worker(import.meta.filename, { workerData: data })
+  worker.on('message', (msg) => console.log('Reply from Thread:', msg))
+} else {
+  const source = workerData
+  parentPort?.postMessage(source.toUpperCase())
+}
+
+// run with `bun threads.ts`
