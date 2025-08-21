af339f12494a9d43135708054d94108f788b2b6f
Edy Segura <edysegura@gmail.com>
2025-08-21 18:03:39 -0300

    refactor: add type annotation for message event in worker

--- nodejs-compatibilty/threads.ts
+++ nodejs-compatibilty/threads.ts
@@ -8,7 +8,9 @@ import {
 if (isMainThread) {
   const data = 'some data'
   const worker = new Worker(import.meta.filename, { workerData: data })
-  worker.on('message', (msg) => console.log('Reply from Thread:', msg))
+  worker.on('message', (msg: string) =>
+    console.log(`Reply from Thread: ${msg}`),
+  )
 } else {
   const source = workerData
   parentPort?.postMessage(source.toUpperCase())
