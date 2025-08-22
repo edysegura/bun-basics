8bd6d66139bcbe147c82779f1bc5706a9a7d82f9
Edy Segura <edysegura@gmail.com>
2025-08-22 16:59:37 -0300

    feat: add happy dom test example

---
+++ test/dom/dom.test.ts
@@ -0,0 +1,10 @@
+// import { GlobalRegistrator } from '@happy-dom/global-registrator'
+// GlobalRegistrator.register()
+
+import { expect, test } from 'bun:test'
+
+test('dom test', () => {
+  document.body.innerHTML = `<button>My button</button>`
+  const button = document.querySelector('button')
+  expect(button?.innerText).toEqual('My button')
+})
