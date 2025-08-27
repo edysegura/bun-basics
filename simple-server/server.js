7e89c7b5b51df024c2b81c7f092b6b84f1038a8f
Edy Segura <edysegura@gmail.com>
2025-08-27 19:30:31 -0300

    refactor: simplify server configuration and improve logging format

--- simple-server/server.js
+++ simple-server/server.js
@@ -1,16 +1,13 @@
 import { serve } from 'bun'
 import homePage from './index.html'
 
-const PORT = 3000
-const ADDRESS = 'localhost'
-
 // This server will serve the HTML file and all its bundled assets.
-serve({
-  port: PORT,
-  hostname: ADDRESS,
+const server = serve({
   routes: {
     '/': homePage,
   },
 })
 
-console.log(`Server is up and running at http://${ADDRESS}:${PORT}`)
+console.log(
+  `Server is up and running at http://${server.hostname}:${server.port}`,
+)
