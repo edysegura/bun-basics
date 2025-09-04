1034dc3cfabfa89ad9b206ebdbe78a8a49752652
Edy Segura <edysegura@gmail.com>
2025-09-04 15:38:38 -0300

    feat: enhance the file example

--- handle-file/index.ts
+++ handle-file/index.ts
@@ -1,6 +1,10 @@
-import { file as fileReader } from 'bun'
+import { file as fileReader, write as fileWriter } from 'bun'
 
 const packageJson = fileReader('../package.json')
 
 console.log('name:', packageJson.name)
 console.log(await packageJson.text())
+
+await fileWriter('./log.txt', `[${Date.now()}] - Hi there!`)
+const fileLog = fileReader('./log.txt')
+console.log('log:', await fileLog.text())
