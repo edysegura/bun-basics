f34287af4540ec1bac06de3342cf4140203aa648
Edy Segura <edysegura@gmail.com>
2025-09-10 21:34:07 -0300

    feat: import JSON file and log its content alongside HTML file

--- import-files/index.js
+++ import-files/index.js
@@ -1,3 +1,8 @@
-import htmlFile from './files/index.html'  with { type: "text" }
+import htmlFile from './files/index.html' with { type: "text" }
+import jsonFile from '../package.json'
 
+console.log('---> HTML File')
 console.log(htmlFile)
+
+console.log('---> JSON File')
+console.log(jsonFile)
