0a33af2ee4591677876e46a0080f427654336a1c
Edy Segura <edysegura@gmail.com>
2025-09-11 17:48:21 -0300

    feat: add YAML configuration file and update index.js to log its content

--- import-files/index.js
+++ import-files/index.js
@@ -1,8 +1,12 @@
 import htmlFile from './files/index.html' with { type: "text" }
-import jsonFile from '../package.json' with { type: "json" }
+import jsonFile from '../package.json'
+import yamlFile from './files/config.yaml'
 
 console.log('---> HTML File')
 console.log(htmlFile)
 
 console.log('---> JSON File')
-console.log(jsonFile)
+console.log(jsonFile.name, 'is private:', jsonFile.private)
+
+console.log('---> YAML File')
+console.log(`${yamlFile.database.host}:${yamlFile.database.port}`)
