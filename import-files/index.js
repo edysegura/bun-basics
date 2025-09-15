0ccb99854f5f8e3919a3438b69fffe312297e7e8
Edy Segura <edysegura@gmail.com>
2025-09-15 18:44:54 -0300

    feat: add TOML file logging and improve console output formatting

--- import-files/index.js
+++ import-files/index.js
@@ -1,12 +1,16 @@
 import htmlFile from './files/index.html' with { type: "text" }
 import jsonFile from '../package.json'
 import yamlFile from './files/config.yaml'
+import tomlFile from './files/config.toml'
 
 console.log('---> HTML File')
 console.log(htmlFile)
 
 console.log('---> JSON File')
-console.log(jsonFile.name, 'is private:', jsonFile.private)
+console.log(jsonFile.name, 'is private:', jsonFile.private, "\n")
 
 console.log('---> YAML File')
-console.log(`${yamlFile.database.host}:${yamlFile.database.port}`)
+console.log(`${yamlFile.database.host}:${yamlFile.database.port}`, "\n")
+
+console.log('---> TOML File')
+console.log(`${tomlFile.database.host}:${tomlFile.database.port}`, "\n")
